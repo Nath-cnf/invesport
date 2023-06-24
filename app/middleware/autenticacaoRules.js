9ffec6acca2038834d2dc78e8dc3953563135b6b
nathalia <rm82338@estudante.fieb.edu.br>
2023-06-24 14:47:34 -0300

    teste

--- app/middleware/autenticacaoRules.js
+++ app/middleware/autenticacaoRules.js
@@ -21,7 +21,7 @@ const validationMiddlewareRules = {
         .isLength({min: 2, max: 2})
         .isIn(["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"
         ])
-        .withMessage("Selecione um estado!"),
+        .withMessage("Insira a sigla do estado!"),
         body("email")
         .isEmail()
         .withMessage("Escreva seu email"),
