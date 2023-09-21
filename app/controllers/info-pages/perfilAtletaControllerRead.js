3df9d36f0f6de5dbf667b489fdb6430320726f9b
Nathalia Fernandes <nathaliafc103@gmail.com>
2023-09-20 23:59:11 -0300

    perfil-atleta

--- app/controllers/info-pages/perfilAtletaControllerRead.js
+++ app/controllers/info-pages/perfilAtletaControllerRead.js
@@ -1,8 +1,15 @@
+const prisma = require("../../../server/database/prismaClient")
+const jwt = require("jsonwebtoken")
+
 class perfilAtletaControllerRead {
-    getPage(req, res) {
+    async getPage(req, res) {
+        const token = req.session.token
+        const { userId } = jwt.decode(token, process.env.SECRET)
+        const usuario = await prisma.usuario.findUnique({where:{id: userId}})
         res.render("pages/perfil-atleta.ejs", {
             data: {
-                page_name: "Invesport"
+                page_name: "Invesport",
+                usuario
             }
         })
     }
