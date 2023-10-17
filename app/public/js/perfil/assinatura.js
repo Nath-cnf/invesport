0ad490025b0bf7d821104affd7f42e535246e7e4
Nathalia Fernandes <nathaliafc103@gmail.com>
2023-10-16 21:06:31 -0300

    assinatura

---
+++ app/public/js/perfil/assinatura.js
@@ -0,0 +1,69 @@
+const stripe = Stripe("pk_live_51NEzE6G905OAea9EXtAoN55BALzufyaCSvC112MSnXeZYzDn1hfnainQ2xXtHDEipFvmD065A3cEx6i7PR1EmpMJ00pOv3JZIL");
+
+const form = document.querySelector("#assinatura-form");
+
+function setMessage(message) {
+	const messageDiv = document.querySelector("#mensagens");
+	messageDiv.innerHTML = message;
+}
+
+const url = new URL(window.location.href);
+const searchParams = new URLSearchParams(url.search);
+const product = searchParams.get("product");
+
+fetch("/pagamento-assinatura", {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json",
+	},
+	body: JSON.stringify({
+		product
+	}),
+}).then(async (res) => {
+	const jsonRes = await res.json();
+
+	if (jsonRes.erro) {
+		setMessage(`Erro de sistema. Por favor tente novamente mais tarde!`);
+		return;
+	}
+
+	const clientSecret = jsonRes.clientSecret;
+	const elements = stripe.elements({
+		clientSecret,
+	});
+
+	const paymentElement = elements.create("payment", {
+		layout: "tabs",
+		fields: {
+			billingDetails: {
+				name: "auto",
+				email: "auto",
+			},
+		},
+	});
+	paymentElement.mount("#cartao-elemento");
+
+	form.addEventListener("submit", async (e) => {
+		e.preventDefault();
+
+		stripe
+			.confirmPayment({
+				elements,
+				confirmParams: {
+					return_url: `${process.env.DOMAIN}compra-efetuada`,
+				},
+			})
+			.then(function (result) {
+				if (result.error) {
+					setMessage(`Pagamento não realizado: ${result.error.message}`);
+				}
+			});
+	});
+});
+
+// This sample only supports a Subscription with payment
+// upfront. If you offer a trial on your subscription, then
+// instead of confirming the subscription's latest_invoice's
+// payment_intent. You'll use stripe.confirmCardSetup to confirm
+// the subscription's pending_setup_intent.
+// See https://stripe.com/docs/billing/subscriptions/trials
