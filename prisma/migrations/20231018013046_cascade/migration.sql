cd8b1765c2c8b789b631a4b74bcf26010c08a54c
Nathalia Fernandes <nathaliafc103@gmail.com>
2023-10-18 12:26:12 -0300

    teste

--- prisma/migrations/20231018013046_cascade/migration.sql
+++ prisma/migrations/20231018013046_cascade/migration.sql
@@ -1,14 +1,14 @@
 -- DropForeignKey
-ALTER TABLE `tarefaclube` DROP FOREIGN KEY `TarefaClube_owner_id_fkey`;
+ALTER TABLE `Tarefaclube` DROP FOREIGN KEY `TarefaClube_owner_id_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `tarefausuario` DROP FOREIGN KEY `TarefaUsuario_owner_id_fkey`;
+ALTER TABLE `Tarefausuario` DROP FOREIGN KEY `TarefaUsuario_owner_id_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `token` DROP FOREIGN KEY `Token_email_fkey`;
+ALTER TABLE `Token` DROP FOREIGN KEY `Token_email_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `usuario` DROP FOREIGN KEY `Usuario_id_esporte_fkey`;
+ALTER TABLE `Usuario` DROP FOREIGN KEY `Usuario_id_esporte_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `Usuario` ADD CONSTRAINT `Usuario_id_esporte_fkey` FOREIGN KEY (`id_esporte`) REFERENCES `Esporte`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
