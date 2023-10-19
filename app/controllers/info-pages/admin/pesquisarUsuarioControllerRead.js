6a365f0e83ffe42672ccef6af665d6a18636701c
Nathalia Fernandes <nathaliafc103@gmail.com>
2023-10-19 01:39:08 -0300

    att pesquisa admin

--- app/controllers/info-pages/admin/pesquisarUsuarioControllerRead.js
+++ app/controllers/info-pages/admin/pesquisarUsuarioControllerRead.js
@@ -1,11 +1,12 @@
 const usuarioModel = require("../../../models/Usuario");
+const clubeModel = require("../../../models/Clube");
 
 class pesquisaUsuarioController {
     async getPage(req, res) {
         const filtroPesquisa = req.query.pesquisar_usuarios;
 
         const usuarios = await usuarioModel.findAllUsersPesquisa(filtroPesquisa);
-
+        const clubes = await clubeModel.findAllClubes("");
         const quantidadeUsuariosPremium = await usuarioModel.countUserPremium();
 
         res.render("pages/admin/home-admin.ejs", {
--- prisma/migrations/20231018215016_add_sobre/migration.sql
+++ prisma/migrations/20231018215016_add_sobre/migration.sql
@@ -1,7 +1,7 @@
 -- AlterTable
-ALTER TABLE `clube` ADD COLUMN `data_inauguracao` DATETIME(3) NULL,
+ALTER TABLE `Clube` ADD COLUMN `data_inauguracao` DATETIME(3) NULL,
     ADD COLUMN `descricao` VARCHAR(255) NULL,
     ADD COLUMN `premium` TINYINT NOT NULL DEFAULT 0;
 
 -- AlterTable
-ALTER TABLE `usuario` ADD COLUMN `idade` INTEGER NULL;
+ALTER TABLE `Usuario` ADD COLUMN `idade` INTEGER NULL;
